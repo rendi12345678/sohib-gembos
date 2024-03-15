6cd6e4944128ffbafb849ce680c7625834536a65
rendi12345678 <hardleberg@gmail.com>
2024-03-15 22:08:23 +0800

    Added Animation

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,9 +1,24 @@
 const body = document.body;
 const toggleButton = document.querySelector("header button");
+const downloadBtn = document.querySelector(".download-btn");
 const darkModeThemeKey = "dark-mode";
 const darkModeThemeButtonText = "Mode Gelap";
 const lightModeThemeButtonText = "Mode Cerah";
 const lightModeThemeKey = "light-mode";
+
+downloadBtn.addEventListener("click", () => {
+  const fileUrl = "../files/kinemaster-terbaru-by-sohib-gembos.zip";
+
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = "kinemaster-terbaru-by-sohib-gembos.zip";
+
+  document.body.appendChild(a);
+
+  a.click();
+
+  document.body.removeChild(a);
+});
 
 toggleButton.addEventListener("click", function () {
   body.classList.toggle(darkModeThemeKey);
@@ -43,4 +58,23 @@ function checkUserTheme() {
   setButtonText(toggleButton, darkModeThemeButtonText);
 }
 
-checkUserTheme();
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+  reveals.forEach((reveal) => {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveal.getBoundingClientRect().top;
+    const revealPoint = 30;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveal.classList.add("active");
+    } else {
+      reveal.classList.remove("active");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", reveal);
+  reveal()
+  checkUserTheme();
+});
